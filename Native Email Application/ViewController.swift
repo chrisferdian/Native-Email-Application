bd2159cc3f9922c8d66180095f1830bb1d8455a1
Chris Ferdian <chris.ferdian@tokiomarine-life.co.id>
2020-10-14 14:49:11 +0700

    first commit

---
+++ Native Email Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Native Email Application
+//
+//  Created by TMLI IT DEV on 13/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
---
+++ Native Email ApplicationTests/Native_Email_ApplicationTests.swift
@@ -0,0 +1,33 @@
+//
+//  Native_Email_ApplicationTests.swift
+//  Native Email ApplicationTests
+//
+//  Created by TMLI IT DEV on 13/10/20.
+//
+
+import XCTest
+@testable import Native_Email_Application
+
+class Native_Email_ApplicationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
